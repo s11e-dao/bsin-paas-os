2d75cbf31f2dee40b0fa35469bebb67edb0d7cde
杨前锦 <1010338399@qq.com>
2024-05-27 00:15:55 +0800

    feat(): add es

---
+++ bsin-server-apps/bsin-server-es/es-domain/src/main/java/me/flyray/bsin/domain/response/SysUserVO.java
@@ -0,0 +1,17 @@
+package me.flyray.bsin.domain.response;
+
+
+import lombok.Data;
+
+/**
+ * @author bolei
+ * @date 2023/9/22
+ * @desc
+ */
+
+@Data
+public class SysUserVO {
+
+    private String token;
+
+}
