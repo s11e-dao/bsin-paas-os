b75896fc1f62ca3ad4c3267de26c94a89a89f579
boleixiongdi <923008946@qq.com>
2024-11-10 11:44:07 +0800

    perf(): 修改oss上传api

--- bsin-server-apps/bsin-server-upms/upms-server/src/main/java/me/flyray/bsin/server/controller/UploadController.java
+++ bsin-server-apps/bsin-server-upms/upms-server/src/main/java/me/flyray/bsin/server/controller/UploadController.java
@@ -5,6 +5,7 @@ import me.flyray.bsin.utils.BsinResultEntity;
 import org.apache.shenyu.client.apidocs.annotations.ApiDoc;
 import org.apache.shenyu.client.apidocs.annotations.ApiModule;
 import org.apache.shenyu.client.springmvc.annotation.ShenyuSpringMvcClient;
+import org.apache.shenyu.plugin.api.result.DefaultShenyuEntity;
 import org.dromara.x.file.storage.core.FileInfo;
 import org.dromara.x.file.storage.core.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -33,12 +37,12 @@ public class UploadController {
      */
     @PostMapping(value = "/aliOssUpload")
     @ApiDoc(desc = "aliOssUpload")
-    public FileInfo aliOssUpload(@RequestPart("file") final MultipartFile file) {
+    public Map aliOssUpload(@RequestPart("file") final MultipartFile file) {
         System.out.println(file);
         FileInfo fileInfo =
                 fileStorageService
                         .of(file)
-                        // .setPath("relativePath/") // 保存到相对路径下，为了方便管理，不需要可以不写
+                        // .setPath("relativePath") // 保存到相对路径下，为了方便管理，不需要可以不写
                         //                        .setObjectId("0") // 关联对象id，为了方便管理，不需要可以不写
                         //                        .setObjectType("0") // 关联对象类型，为了方便管理，不需要可以不写
                         //                        .setPlatform("aliyun-oss") // 使用指定的存储平台
@@ -46,7 +50,11 @@ public class UploadController {
                         // 保存一些属性，可以在切面、保存上传记录、自定义存储平台等地方获取使用，不需要可以不写
                         .upload(); // 将文件上传到对应地方
         //        return fileInfo == null ? "上传失败！" : fileInfo.getUrl();
-        return fileInfo;
+        Map resultMap = new HashMap<>();
+        resultMap.put("code", 0);
+        resultMap.put("message", "操作成功");
+        resultMap.put("data", fileInfo);
+        return resultMap;
     }
 
 }
