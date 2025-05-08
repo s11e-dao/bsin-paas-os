b04128d846b5ed9f71034bf07bc7b35768279355
boleixiongdi <923008946@qq.com>
2025-05-08 15:44:52 +0800

    feat(): add 数据中台router

--- bsin-ui-apps/bsin-ui-data-warehouse/config/routes.ts
+++ bsin-ui-apps/bsin-ui-data-warehouse/config/routes.ts
@@ -36,6 +36,50 @@ const routes = [
         name: '非基座运行',
         path: '/uncontainer',
         component: '@/pages/uncontainer'
+      },
+      // **************************数据模型**************************
+      {
+        name: '数据模型',
+        path: '/uncontainer',
+        component: '@/pages/uncontainer'
+      },
+      // **************************数据地图**************************
+      {
+        name: '数据地图',
+        path: '/uncontainer',
+        component: '@/pages/uncontainer'
+      },
+      // **************************数据治理**************************
+      {
+        name: '元数据管理',
+        path: '/uncontainer',
+        component: '@/pages/uncontainer'
+      },
+      {
+        name: '数据血缘',
+        path: '/uncontainer',
+        component: '@/pages/uncontainer'
+      },
+      {
+        name: '数据质量',
+        path: '/uncontainer',
+        component: '@/pages/uncontainer'
+      },
+      {
+        name: '生命周期',
+        path: '/uncontainer',
+        component: '@/pages/uncontainer'
+      },
+      {
+        name: '数据价值计量',
+        path: '/uncontainer',
+        component: '@/pages/uncontainer'
+      },
+      // **************************数据服务**************************
+      {
+        name: '数据服务',
+        path: '/uncontainer',
+        component: '@/pages/uncontainer'
       }
     ]
   }
