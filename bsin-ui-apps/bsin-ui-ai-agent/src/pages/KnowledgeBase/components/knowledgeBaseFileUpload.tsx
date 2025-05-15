9cc3590127da164338ca2de2e1815e9abc1886f8
boleixiongdi <923008946@qq.com>
2025-05-15 16:03:18 +0800

    feat(): feat file upload

--- bsin-ui-apps/bsin-ui-ai-agent/src/pages/KnowledgeBase/components/knowledgeBaseFileUpload.tsx
+++ bsin-ui-apps/bsin-ui-ai-agent/src/pages/KnowledgeBase/components/knowledgeBaseFileUpload.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
-import { Button, message, Steps, theme } from 'antd';
-import { BackwardOutlined } from '@ant-design/icons'
+import { Button, message, Steps, theme, Upload } from 'antd';
+import type { UploadProps } from 'antd';
+import { BackwardOutlined, InboxOutlined } from '@ant-design/icons'
 
+const { Dragger } = Upload;
 
 const steps = [
     {
@@ -14,6 +16,26 @@ const steps = [
         title: '确认上传',
     },
 ];
+
+const props: UploadProps = {
+    name: 'file',
+    multiple: true,
+    action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
+    onChange(info) {
+        const { status } = info.file;
+        if (status !== 'uploading') {
+            console.log(info.file, info.fileList);
+        }
+        if (status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully.`);
+        } else if (status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+        }
+    },
+    onDrop(e) {
+        console.log('Dropped files', e.dataTransfer.files);
+    },
+};
 
 const KnowledgeBaseFileUpload: React.FC = ({
     routeChange,
@@ -59,7 +81,16 @@ const KnowledgeBaseFileUpload: React.FC = ({
                 <Steps current={current} items={items} />
             </div>
             <div style={contentStyle}>
-                内容区
+                <Dragger {...props}>
+                    <p className="ant-upload-drag-icon">
+                        <InboxOutlined />
+                    </p>
+                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                    <p className="ant-upload-hint">
+                        Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+                        banned files.
+                    </p>
+                </Dragger>
             </div>
             <div style={{ marginTop: 24, display: 'flex', justifyContent: 'flex-end' }}>
                 {current > 0 && (
