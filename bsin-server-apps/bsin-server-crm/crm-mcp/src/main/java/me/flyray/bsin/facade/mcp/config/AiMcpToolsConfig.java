eced197131e0b8e8560695e8ec157b62090e9796
boleixiongdi <923008946@qq.com>
2025-05-30 16:12:09 +0800

    perf(): 更新ai依赖配置

--- bsin-server-apps/bsin-server-crm/crm-mcp/src/main/java/me/flyray/bsin/facade/mcp/config/AiMcpToolsConfig.java
+++ bsin-server-apps/bsin-server-crm/crm-mcp/src/main/java/me/flyray/bsin/facade/mcp/config/AiMcpToolsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AiToolsConfig {
+public class AiMcpToolsConfig {
 
     @Bean
     ToolCallbackProvider tools(OpenMeteoService openMeteoService) {
